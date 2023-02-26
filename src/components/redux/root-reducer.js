669623a66aab16a2994ced9c2e707e458c2d9b66
oksahil <oksana.khylyk@gmail.com>
2023-02-26 20:23:45 +0100

    fix

---
+++ src/components/redux/root-reducer.js
@@ -0,0 +1,20 @@
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
+import phonesReducer from "./phones/phones-slice";
+import filterReducer from "./filter/filter-slice";
+
+const rootReducer = combineReducers({
+    phones: phonesReducer,
+    filter: filterReducer,
+})
+
+const persistConfig = {
+  key: 'phonebook',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default persistedReducer;
